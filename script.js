4b8eb97cf3a2049e9d3a8bbfb0d56c2e1386c05a
rosaTome <tomerosalinda@gmail.com>
2024-03-14 20:36:29 +0000

    Quesion 3 modified with if statements instead for added clarity

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // isNumberPositive(-1); // returns false
 // isNumberPositive(10); // returns true
 
+// Function takes input number and returns true (positive) or false (negative)
 function isNumberPositive(number) {
     if (number > 0) {
         return true
@@ -9,20 +10,19 @@ function isNumberPositive(number) {
         return false;
     }
 }
-
 console.log(isNumberPositive(-0));
 console.log(isNumberPositive(10));
 
 // 2. Write a function that takes a number of days and converts it into an age.
 // convertDaysToAge(3650); // returns 10
 // convertDaysToAge(6570); // returns 18
-
+ 
 function convertDaysToAge(days) {
     const daysInYear = 365;
+    // divides number of days by number days in a year 
     const age = days / daysInYear;
     return age;
 }
-
 console.log(convertDaysToAge(3650));
 console.log(convertDaysToAge(6570));
 
@@ -31,14 +31,15 @@ console.log(convertDaysToAge(6570));
 // getLargestNumber(6,2,3); // returns 6
 
 function getLargestNumber(num1, num2, num3) {
+    // checks for largest numbers out of the three numbers using if statements 
     if (num1 >= num2 && num1 >= num3) {
         return num1;
     }
-
-    if (num2 >= num3) {
+    else if (num2 >= num1 && num2 >= num3) {
         return num2
+    } else {
+        return num3;
     }
-    return num3;
 }
 console.log(getLargestNumber(2, 1, 4));
 console.log(getLargestNumber(6, 2, 3));
@@ -47,8 +48,13 @@ console.log(getLargestNumber(6, 2, 3));
 // getLastName([”Charlie”, “Rob”, “Andy”]); // returns “Andy”
 // getLastName(["Ash","Stu"]); // returns "Stu"
 
-
+function getLastName(names) {
+    return names[names.length - 1];
+}
+console.log(getLastName(["Charlie", "Rob", "Andy"]));
+console.log(getLastName(["Ash", "Stu"]));
 
 // 5. Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
 // allNumbersPositive([2,4,5]); // returns true
 // allNumbersPositive([-5,4,6]); // returns false
+
